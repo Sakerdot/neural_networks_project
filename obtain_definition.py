750c44cbf452dde77d21f0339b33cb27069c58df
jmvallejop1 <jmvallejop1@gmail.com>
2020-01-23 17:49:11 +0100

    definition of function

--- obtain_definition.py
+++ obtain_definition.py
@@ -2,16 +2,17 @@ import requests
 import string
 from nltk.tokenize import word_tokenize
 
-Output: ['God', 'is', 'Great', '!', 'I', 'won', 'a', 'lottery', '.']
-#Anadir la URL de la pagina para obtener las definiciones
-resp = requests.get('http://api.urbandictionary.com/v0/define?term=O_o')
-res = resp.json()
-if len(res["list"]) != 0:
-    first_def = res["list"][0]["definition"]
-    first_def_wo_definition = first_def.translate(str.maketrans('', '', string.punctuation))
-    tokenized_definition = word_tokenize( first_def_wo_definition)
-    print(tokenized_definition)
-else:
-    print("No definition")
+def obtain_definition(word):
+    url = "http://api.urbandictionary.com/v0/define?term=" + word
+    resp = requests.get(url)
+    res = resp.json()
+    if len(res["list"]) != 0:
+        first_def = res["list"][0]["definition"]
+        first_def_wo_definition = first_def.translate(str.maketrans('', '', string.punctuation))
+        tokenized_definition = word_tokenize( first_def_wo_definition)
+        return tokenized_definition
+    else:
+        return []
 
-    
+# a = obtain_definition("O_o")
+# print(a)
